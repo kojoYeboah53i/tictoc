db5f81ac34ee4cdda028184be7ef2899540625bf
Isaac <yeboah.determined.isaac@gmail.com>
2020-12-06 13:43:07 +0000

    click

--- react-begin/src/index.js
+++ react-begin/src/index.js
@@ -5,8 +5,9 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick= {function (){alert("clicked");}}>
+          {this.props.value}
+          {/* console.log(this.props.value) */}
         </button>
       );
     }
@@ -14,7 +15,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square  value={i} />;
     }
   
     render() {
